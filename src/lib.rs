bf2a7384a7b47e407c9ba5ba823a87d7c319bddb
Olof Johansson <olof@lixom.net>
2022-08-28 23:19:57 -0700

    More cleanups, move longlat into middle of square, more tests
    Signed-off-by: Olof Johansson <olof@lixom.net>

--- src/lib.rs
+++ src/lib.rs
@@ -17,36 +17,74 @@ use self::simple_error::SimpleError;
 // Note that the enumeration begins at south pole (so 90 degrees off on latitude) and is
 // unsigned positive, so needs to be subtracted by 180 to get +/- longitude.
 
-static LONG_OFFSET: f32 = 180.0;
-static LAT_OFFSET: f32 = 90.0;
+static LONG_OFFSET: f64 = 180.0;
+static LAT_OFFSET: f64 = 90.0;
 
-static LONG_F: f32 = 20.0;
-static LAT_F: f32 = 10.0;
-static LONG_SQ: f32 = 2.0;
-static LAT_SQ: f32 = 1.0;
-static LONG_SSQ: f32 = 5.0 / 60.0;
-static LAT_SSQ: f32 = 2.5 / 60.0;
-static LONG_ESQ: f32 = 30.0 / 60.0 / 60.0;
-static LAT_ESQ: f32 = 15.0 / 60.0 / 60.0;
-static LONG_SESQ: f32 = 1.25 / 60.0 / 60.0;
-static LAT_SESQ: f32 = 0.625 / 60.0 / 60.0;
+static LONG_F: f64 = 20.0;
+static LAT_F: f64 = 10.0;
+static LONG_SQ: f64 = 2.0;
+static LAT_SQ: f64 = 1.0;
+static LONG_SSQ: f64 = 5.0 / 60.0;
+static LAT_SSQ: f64 = 2.5 / 60.0;
+static LONG_ESQ: f64 = 30.0 / 60.0 / 60.0;
+static LAT_ESQ: f64 = 15.0 / 60.0 / 60.0;
+static LONG_SESQ: f64 = 1.25 / 60.0 / 60.0;
+static LAT_SESQ: f64 = 0.625 / 60.0 / 60.0;
 
-static LONG_MULT: [f32; 5] = [LONG_F, LONG_SQ, LONG_SSQ, LONG_ESQ, LONG_SESQ];
-static LAT_MULT: [f32; 5] = [LAT_F, LAT_SQ, LAT_SSQ, LAT_ESQ, LAT_SESQ];
+static LONG_MULT: [f64; 5] = [LONG_F, LONG_SQ, LONG_SSQ, LONG_ESQ, LONG_SESQ];
+static LAT_MULT: [f64; 5] = [LAT_F, LAT_SQ, LAT_SSQ, LAT_ESQ, LAT_SESQ];
 
-pub fn grid_to_longlat(grid: &str) -> Result<(f32, f32), SimpleError>
+pub fn grid_to_longlat(grid: &str) -> Result<(f64, f64), SimpleError>
 {
-    let vals: Vec<u32> = "AA00aa00AA".chars().zip(grid.chars()).map(
-        | (t, c) | (c.to_ascii_uppercase() as u32) - (t.to_ascii_uppercase() as u32)).collect();
-    let long: f32 = vals.iter().step_by(2).zip(LONG_MULT).map(| (&v, m) | v as f32 * m ).sum();
-    let lat: f32 = vals.iter().skip(1).step_by(2).zip(LAT_MULT).map(| (&v, m) | v as f32 * m ).sum();
+    // Validate alpha/digit format
+    // FIXME: Actual values should be A-R 0-9 a-x 0-9 A-X
+    let d = | a: char | a.is_ascii_digit();
+    let l = | a: char | a.is_ascii_alphabetic();
+    let checks = [l, l, d, d, l, l, d, d, l, l];
+    let check = grid.chars().zip(checks).map(|(c, lmb)| lmb(c)).collect::<Vec<bool>>();
+
+    // If any of them are false, we've got an invalid grid string
+    if check.iter().filter(|b| ! *b).count() != 0 {
+        return Err(SimpleError::new("Invalid grid format"));
+    }
+
+    // Also make sure the length is even (and not 2)
+    match grid.len() {
+        4 | 6 | 8 | 10 => {},
+        _ => return Err(SimpleError::new("Invalid grid length")), 
+    }
+
+    // Now it's just a matter of calculating the offsets from the grid
+    let vals: Vec<u32> = "AA00AA00AA".chars().zip(grid.chars()).map(
+        | (t, c) | (c.to_ascii_uppercase() as u32) - (t as u32)).collect();
+
+    // And multiplying each of them with their per-unit value
+    let long: f64 = vals.iter().step_by(2).zip(LONG_MULT).map(| (&v, m) | v as f64 * m ).sum();
+    let lat: f64 = vals.iter().skip(1).step_by(2).zip(LAT_MULT).map(| (&v, m) | v as f64 * m ).sum();
+
+    // Move the returned value into the middle of the precision given.
+    // This avoids imprecision due to rounding if doing grid->longlat->grid
+    // (We do this in a unit testcase)
+    let idx = grid.len() / 2 - 1;
+    let long = long + LONG_MULT[idx] / 2.0;
+    let lat = lat + LAT_MULT[idx] / 2.0;
+
+    // Finally, adjust for origin offsets
     Ok((long - LONG_OFFSET, lat - LAT_OFFSET))
 }
 
-pub fn longlat_to_grid(long: f32, lat: f32, precision: usize) -> Result<String, SimpleError>
+pub fn longlat_to_grid(long: f64, lat: f64, precision: usize) -> Result<String, SimpleError>
 {
-    fn charoff(base: char, off: u32) -> Option<char> {
-        std::char::from_u32(base as u32 + off)
+    let charoff = | base: char, off: u32 | std::char::from_u32(base as u32 + off);
+
+    // It only makes sense to have 4+ even number of characters in a grid square
+    match precision {
+        4 | 6 | 8 | 10 => {},
+        _ => return Err(SimpleError::new("Invalid grid length {precision}")),
+    }
+
+    if long > 180.0 || long < -180.0 || lat < -180.0 || lat > 180.0 {
+        return Err(SimpleError::new("Invalid long/lat"));
     }
 
     // Do the math to calculate each position, per the w8bh website
@@ -64,11 +102,7 @@ pub fn longlat_to_grid(long: f32, lat: f32, precision: usize) -> Result<String, 
     vals.push(long % LONG_ESQ / LONG_SESQ);
     vals.push(lat % LAT_ESQ / LAT_SESQ);
 
-    // It only makes sense to have 4+ even number of characters in a grid square
-    match precision {
-        0 | 4 | 6 | 8 | 10 => vals.truncate(precision),
-        _ => return Err(SimpleError::new("Invalid grid length {precision}")),
-    }
+    vals.truncate(precision);
 
     let grid: Option<String> = "AA00aa00AA".chars().zip(vals).map(
         | (b, o) | charoff(b, o as u32) ).collect();
@@ -77,7 +111,6 @@ pub fn longlat_to_grid(long: f32, lat: f32, precision: usize) -> Result<String, 
         None => Err(SimpleError::new("Failed to generate grid"))
     }
 }
-
 
 
 #[cfg(test)]
@@ -91,9 +124,10 @@ mod tests {
         };
     }
 
+    // These values come out of the PDF referenced at the top of this file
     static TEST_GRID: &str = &"FM18lv53SL";
-    static TEST_LONG: f32 = -77.035278;
-    static TEST_LAT: f32 = 38.889484;
+    static TEST_LONG: f64 = -77.035278;
+    static TEST_LAT: f64 = 38.889484;
 
     fn precision_n(n: usize) {
         let grid = longlat_to_grid(TEST_LONG, TEST_LAT, n).unwrap();
@@ -104,53 +138,75 @@ mod tests {
     }
 
     #[test]
-    fn precision_10() {
-        precision_n(10);
-    }
+    fn precision_10() { precision_n(10); }
 
     #[test]
-    fn precision_4() {
-        precision_n(4);
-    }
+    fn precision_8() { precision_n(8); }
+
+    #[test]
+    fn precision_6() { precision_n(6); }
+
+    #[test]
+    fn precision_4() { precision_n(4); }
 
     #[test]
     fn precision_inval() {
-        let grid = longlat_to_grid(-77.035278, 38.889484, 5);
+        let grid = longlat_to_grid(TEST_LONG, TEST_LAT, 5);
+        assert!(grid.is_err());
+    }
+
+    #[test]
+    fn precision_inval_lat() {
+        let grid = longlat_to_grid(TEST_LONG, 921.0, 10);
+        assert!(grid.is_err());
+    }
+
+    #[test]
+    fn precision_inval_long() {
+        let grid = longlat_to_grid(-201.0, TEST_LAT, 10);
         assert!(grid.is_err());
     }
 
     fn longlat_n(n: usize) {
         let mut grid_in = String::from(TEST_GRID);
         grid_in.truncate(n);
+
         let ll = grid_to_longlat(&grid_in.as_str());
         assert!(!ll.is_err());
-        let (long, lat) = ll.unwrap();
-        println!("lat {} long {}", lat, long);
+
         // Make sure it's within the margin of error of the smallest field
-        assert_delta!(long, -77.035278, LONG_MULT[4]);
-        assert_delta!(lat, 38.889484, LAT_MULT[4]);
+        let (long, lat) = ll.unwrap();
+        assert_delta!(long, TEST_LONG, LONG_MULT[4]);
+        assert_delta!(lat, TEST_LAT, LAT_MULT[4]);
+
+        // Let's convert it back to grid and compare
         let grid = longlat_to_grid(long, lat, n).unwrap();
-        println!("grid_in {} grid {}", grid_in, grid);
+        assert_eq!(grid_in, grid);
     }
 
     #[test]
-    fn longlat10() {
-        longlat_n(10);
-    }
+    fn longlat10() { longlat_n(10); }
 
     #[test]
-    fn longlat8() {
-        longlat_n(8);
-    }
+    fn longlat8() { longlat_n(8); }
 
     #[test]
-    fn longlat6() {
-        longlat_n(6);
-    }
+    fn longlat6() { longlat_n(6); }
 
     #[test]
-    fn longlat4() {
-        longlat_n(4);
+    fn longlat4() { longlat_n(4); }
+
+    #[test]
+    fn longlat_invalid() {
+        let ret = grid_to_longlat("AI021");
+        assert!(ret.is_err());
+        let ret = grid_to_longlat("AIA2");
+        assert!(ret.is_err());
+        let ret = grid_to_longlat("🤷I00");
+        assert!(ret.is_err());
+        let ret = grid_to_longlat("AA00AA00AA00");
+        assert!(ret.is_err());
+        let ret = grid_to_longlat("AA00AA00AA");
+        assert!(! ret.is_err());
     }
 }
-
